b9440ac99ab633d48ad64f3b702941ec37e438e6
shehzad <shehzaddiqbal@gmail.com>
2022-07-04 18:52:13 +0500

    chp 5 pg 98

---
+++ chapter5.cpp
@@ -0,0 +1,21 @@
+//page 98
+#include <iostream>
+#include <conio>
+
+using namespace std;
+
+void main(){
+	int a[k], k, total;
+	float avg;
+	for (k=0; k<5; k++){
+		cout<<"Enter a number: ";
+		cin>>a[k];
+	}
+	total=0;
+	for(k=0; k<5; k++)
+		total=total+a[k];
+	avg=total/5;
+	cout<<"\nTotal="<<total<<endl;
+	cout<<"Average="<<avg<<endl;
+	getch();
+}
