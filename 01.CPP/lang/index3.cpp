9704fa9eaede3451b257a1e0be2b873d661c63b8
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-07 07:34:05 +0500

    C++ lang

---
+++ 01.CPP/lang/index3.cpp
@@ -0,0 +1,901 @@
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
+// From Sir Inzamam repo for practice
+#include <iostream>
+#include <string>
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+using namespace std;
+
+int main()
+{
+
+  string phase;
+
+  cout << "Enter string: ";
+  getline(cin, phase);
+
+  phase.append(1, ' ');
+
+  string stream[1000];
+  int lastSpace = 0;
+  int streamCount = 0;
+  int phaseLen = phase.length();
+
+  for (int i = 0; i < phaseLen; i++)
+  {
+
+    //	cout << "i = "<< i << " out of " << phase.length();
+    // cout << " " << phase.at(i) << endl ;
+
+    if (phase.at(i) == ' ')
+    { // ddddd
+
+      //	cout << "length is : " << i << endl ;
+      string temp = "";
+
+      for (int j = lastSpace; j < i; j++)
+      {
+
+        temp.append(1, phase.at(j));
+      }
+      stream[streamCount] = temp;
+      stream[streamCount + 1] = "space";
+      streamCount += 2;
+      lastSpace = i;
+      temp = "";
+    }
+  }
+
+  for (int i = 0; i < streamCount - 1; i++)
+  {
+    cout << i << " = " << stream[i] << endl;
+  }
+
+  return 0;
+}
