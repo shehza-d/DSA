1c59363ff45b9386e31320d391cb63914059c56d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-09 06:25:10 +0500

    es5 vs 6

--- 02.HackerRank/01.arraySum.js
+++ 02.HackerRank/01.arraySum.js
@@ -1,16 +1,18 @@
 //$$$$$$$$$$$$$$$$$$$$$$$$$$ First Problem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 //Given an array of integers, find the sum of its elements. Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+const arr = [2, 4, 2, 9, 6, 5, 9];
 
-function simpleArraySum(ar) {
-  let result;
+// OLD CODE ES5
+function arraySum(ar) {
+  let result = 0;
   for (let i = 0; i < ar.length; i++) {
     result = result + ar[i];
   }
   return result;
 }
 
-// NEW CODE
-const arr = [2, 4, 2, 9, 6, 5, 9];
-let total = 0;
-arr.map((i) => (total += i));
-// console.log(total);
+// NEW CODE ES6+
+const arraySum = (arr) => arr.reduce((prev, curr) => prev + curr);
+
+// log
+console.log(arraySum(arr));
