2db7c5639436548f3bbfbca091914b112e177b49
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-12 07:11:36 +0500

    Dsa Notes

--- DSA-Notes.cpp
+++ DSA-Notes.cpp
@@ -1,11 +1,16 @@
 /*
 
-# dsa questions
+# DSA Questions
 
+12-Apr-2023
+[dsa mistakes](https://youtu.be/6XHg_TBDaqA)
 01. before Question write pseudo code
 02. give atleast 30-40 mint to each question even if you don't find the solution
 03. if you find the solution ALWAYS LOOK FOR MOST OPTIMIZED SOLUTION!!
 04.  dont memorize code
 05. confidence rakho(jisko yaqeen hy wo nikal lega company)
 06. easy question phle for confidence
+07. Participate in Hackathons, competitions and contests(leetCode) (even if don't know that tech)
+08. REVISION ! (notes banao)
+
 */
