213dbf44f6a05332cfc667039019a5e8b857190f
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-12 07:02:21 +0500

    DSA Notes

--- DSA-Notes.cpp
+++ DSA-Notes.cpp
@@ -5,6 +5,7 @@
 01. before Question write pseudo code
 02. give atleast 30-40 mint to each question even if you don't find the solution
 03. if you find the solution ALWAYS LOOK FOR MOST OPTIMIZED SOLUTION!!
-
-
+04.  dont memorize code
+05. confidence rakho(jisko yaqeen hy wo nikal lega company)
+06. easy question phle for confidence
 */
