dd68700a9df645e4f5b028989009ad37cfea0f59
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-24 16:37:15 +0500

    all questions done

--- 07.Tasks-for-Uni-students/01.task.js
+++ 07.Tasks-for-Uni-students/01.task.js
@@ -1,7 +1,7 @@
 const question2 = (arr) => {
   let result = [];
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr[0].length; i++) {
     for (let j = 0; j < arr.length; j++) {
       result.push(arr[j][i]);
     }
@@ -9,9 +9,44 @@ const question2 = (arr) => {
   return result;
 };
 
+const question3 = (arr) => {
+  let result = [];
+
+  for (let i = 0; i < arr[0].length; i++) {
+    result[i] = [];
+    for (let j = 0; j < arr.length; j++) {
+      result[i].push(arr[j][i]);
+    }
+  }
+  return result;
+};
+
+const question4 = (arr) => {
+  let result = [];
+
+  const isPrime = (num) => {
+    num = parseInt(num);
+    if (num < 1) return false;
+    if (num === 1) return null;
+
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) return false;
+    }
+    return true;
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < arr[i].length; j++) {
+      result[i].push(isPrime(arr[i][j]));
+    }
+  }
+  return result;
+};
+
 // Testing
 console.log(
-  question2([
+  question4([
     [12, -3, 2, 14],
     [11, -2, -8, 4],
     [3, 0, 0, -5],
