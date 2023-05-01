b2df3d6218acec94b7a596226f91eb66762eb1f6
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-02 03:00:38 +0500

    done

--- 04.LEET-CODE/07.decodeCipherText.ts
+++ 04.LEET-CODE/07.decodeCipherText.ts
@@ -1,20 +1,23 @@
 // https://leetcode.com/problems/decode-the-slanted-ciphertext/
-const rot13 = (str) => {
+//not done
+
+const decodeCiphertext = (str, rows) => {
   let result = "";
   let alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   for (let i = 0; i < str.length; i++) {
     let index = alphabets.indexOf(str[i]);
-    if (index === -1) {
-      result += str[i];
-      continue;
-    }
+    // if (index === -1) {
+    //   result += str[i];
+    //   continue;
+    // }
 
-    index += 13;
+    index += rows;
     if (index >= 26) index -= 26;
 
     result += alphabets[index];
   }
+
   return result;
 };
-console.log(rot13("SERR PBQR PNZC!#"));
+console.log(decodeCiphertext("iveo    eed   l te   olc", 4));
