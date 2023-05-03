7cb6cc4d42afa56a7ea13ffc1753beeb987c60b7
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-03 14:57:34 +0500

    q2 added

--- 08.Interview_questions/tezeract.js
+++ 08.Interview_questions/tezeract.js
@@ -1,4 +1,7 @@
 // Q1 split without builtin method
 // string ko space se split karky arr ma daalne hy
+const str = "This is my new String here!";
 
 // Q2
+// https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements
+const arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
