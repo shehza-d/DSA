7b6d97104e1cf74870c674c95ffbc2a3a08f0db0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-03 14:53:50 +0500

    interview question added and renamed

---
+++ 08.Interview_questions/owais.js
@@ -0,0 +1,20 @@
+const  Arrays  = [
+    { id: 1, title: "Title 1" },
+    { id: 2, title: "Title 2" },
+    { id: 3, title: "Title 3" },
+];
+
+const postsArray = [
+    { id: 1, post: "Post 1" },
+    { id: 3, post: "Post 3" },
+    { id: 2, post: "Post 2" },
+];
+
+// //Expected output
+// [
+//     { "id": 1, "title": "Title 1", "post": "Post 1" },
+//     { "id": 2, "title": "Title 2", "post": "Post 2" }, 
+//     { "id": 3, "title": "Title 3", "post": "Post 3" }
+// ]
+
+// Q2 print number from 99 to 0 without loop
---
+++ 08.Interview_questions/tezeract.js
@@ -0,0 +1,4 @@
+// Q1 split without builtin method
+// string ko space se split karky arr ma daalne hy
+
+// Q2
