804bb492a263f2f7a5424756125b30570e4e0f67
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-18 12:17:34 +0500

    day 12

--- 09.30_day_of_JS/13.promisePool.ts
+++ 09.30_day_of_JS/13.promisePool.ts
@@ -1,1 +1,11 @@
 // https://leetcode.com/problems/promise-pool
+
+const promisePool = (functions, n) => {};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
+// function promisePool(functions: F[], n: number): Promise<any> {}
+// type F = () => Promise<any>;
