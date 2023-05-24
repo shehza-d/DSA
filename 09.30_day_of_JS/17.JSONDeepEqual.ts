2ca26a67d16e4dbafd63cb1e404c025e0e3ba065
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-24 17:02:00 +0500

    day 17 json

--- 09.30_day_of_JS/17.JSONDeepEqual.ts
+++ 09.30_day_of_JS/17.JSONDeepEqual.ts
@@ -1,9 +1,6 @@
 // https://leetcode.com/problems/json-deep-equal/
 
-// const areDeeplyEqual = (o1: Object, o2: Object): boolean => {
-const areDeeplyEqual = (o1, o2) => {
-  console.log(o2.length);
-
+const areDeeplyEqual = (o1: Object, o2: Object): boolean => {
   if (typeof o1 !== "object") return JSON.stringify(o1) === JSON.stringify(o2);
   const sortableObj = (obj) =>
     Object.fromEntries(
@@ -22,16 +19,10 @@ const areDeeplyEqual = (o1, o2) => {
 // const o1 = { y: 2, x: 1 },  o2 = { x: 1, y: 2 }; //true
 // const o1 = { x: null, L: [1, 2, 3] },  o2 = { x: null, L: ["1", "2", "3"] };// false
 // const o1 = true,  o2 = false; //false
-const o1 = [1],
-  o2 = { "0": 1 }; //false
+// const o1 = [1],  o2 = { "0": 1 }; //false
 
 console.log(areDeeplyEqual(o1, o2));
 
-/**
- * @param {any} o1
- * @param {any} o2
- * @return {boolean}
- */
 const areDeeplyEqual = (o1, o2) => {
   if (o1 === o2) return true;
 
