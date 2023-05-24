1311a5bd4f887358545b4b63822e27f171fb3649
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-24 16:08:28 +0500

    day 16

--- 09.30_day_of_JS/16.throttle.ts
+++ 09.30_day_of_JS/16.throttle.ts
@@ -1,1 +1,35 @@
 // https://leetcode.com/problems/throttle/
+
+//undone
+// function throttle(fn: F, t: number): F {
+//   return function (...args) {};
+// }
+
+const throttle = (fn, t) => {
+  let timerId;
+  let lastArgs;
+  let shouldCall = true;
+
+  const execute = () => {
+    if (shouldCall && lastArgs) {
+      fn(...lastArgs);
+      lastArgs = null;
+      shouldCall = false;
+      setTimeout(() => {
+        shouldCall = true;
+        execute();
+      }, t);
+    }
+  };
+
+  return (...args) => {
+    lastArgs = args;
+    execute();
+  };
+};
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
+// type F = (...args: any[]) => void
