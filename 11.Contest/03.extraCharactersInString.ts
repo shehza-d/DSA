7d995660ac0bd157d6c3e7f7f3eddd9552fd3bd4
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-27 23:02:08 +0500

    trying medium problem of contest

--- 11.Contest/03.extraCharactersInString.ts
+++ 11.Contest/03.extraCharactersInString.ts
@@ -1,4 +1,5 @@
 // const minExtraChar = (s: string, dictionary: string[]): number => {
+//undone
 const minExtraChar = (s, dictionary) => {
   let result = s.length;
   s = s.split("").sort().join("");
@@ -21,3 +22,6 @@ const s = "leetscode",
 // const  s = "sayhelloworld", dictionary = ["hello","world"] //3
 
 console.log(minExtraChar(s, dictionary));
+
+// https://leetcode.com/problems/extra-characters-in-a-string/solutions/3568827/bottom-up-dp/
+// https://leetcode.com/problems/extra-characters-in-a-string/solutions/3568582/2-approach-dp-memoization-trie-approach-detailed-explanation/
