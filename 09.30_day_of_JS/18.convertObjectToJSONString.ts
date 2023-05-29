972349ad6555954149549fad2422f60aee69d246
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-30 01:48:28 +0500

    jsonStringify problem solved

--- 09.30_day_of_JS/18.convertObjectToJSONString.ts
+++ 09.30_day_of_JS/18.convertObjectToJSONString.ts
@@ -1,48 +1,27 @@
-// const jsonStringify = (obj: any): string => {
-const jsonStringify = (obj) => {
-  let result = "";
-  const checkValue = (value) => {
-    if (
-      value === null ||
-      typeof value === "boolean" ||
-      typeof value === "number" ||
-      typeof value === "undefined"
-    ) {
-      return String(value);
-    }
-  };
+const jsonStringify = (value: any): string => {
+  if (typeof value === "string") return `"${value}"`;
+  if (typeof value === "number" || typeof value === "boolean" || value === null)
+    return String(value);
 
-  result += checkValue(obj);
-
-  if (Array.isArray(obj)) {
-    result+= `[${obj.map((item) => checkValue(item))}]`;
+  if (Array.isArray(value)) {
+    if (!value.length) return "[]";
+    const items = value.map((item) => jsonStringify(item)).join(",");
+    return `[${items}]`;
   }
-  //   if(obj)
-  //   let result = `{`;
-  //   //   console.log("keys order", Object.keys(obj));
-  //   for (const key in obj) {
-  //     if (typeof obj[key] === "object") {
-  // 		result+=`"${key}":`
-  //       result += jsonStringify(obj[key]);
-  // 	  continue
-  //     // }else if (typeof obj[key] === ""){
-  // 	// 	result+=`[${}]`
-  // 	}
-  //     if (typeof obj[key] === "string") {
-  //       result += `"${key}":"${obj[key]}",`;
-  //       continue;
-  //     }
-  //     result += `"${key}":${obj[key]},`;
-  //   }
-  //   console.log("result", result.slice(0, -1));
-  //   result = result.slice(0, -1);
-  return `${result}`;
+
+  if (typeof value === "object") {
+    const keys = Object.keys(value);
+    if (!keys.length) return "{}";
+    const items = keys.map((key) => `"${key}":${jsonStringify(value[key])}`);
+    return `{${items.join(",")}}`;
+  }
 };
 
-// const object = { y: 1, x: 2 };
-// const object = { a: "str", b: -12, c: true, d: null };
-// const object = { key: { a: 1, b: [{}, null, "Hello"] } };
-// const object = null
-const object = [1,2,'sheh']
+const object = { y: 1, x: 2 };
+const object = { a: "str", b: -12, c: true, d: null };
+const object = { key: { a: 1, b: [{}, null, "Hello", []] } };
+const object = () => {};
+const object = null;
+const object = [1, 2, "shehzad"];
 
 console.log(jsonStringify(object));
