468b1409fb2c56e0c270418eb2232fb1970bf455
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-09 00:59:40 +0500

    biweekly 108 failed

---
+++ 11.Contest/04.biweekly-contest-108/01.longest-alternating-subarray.ts
@@ -0,0 +1,12 @@
+// const alternatingSubarray=(nums: number[]): number => {
+const alternatingSubarray = (nums) => {
+  let maxLength = -1;
+
+  return maxLength >= 2 ? maxLength : -1;
+};
+
+const nums = [2, 3, 4, 3, 4]; // 4
+const nums = [4, 5, 6]; // 2
+const nums = [21, 9, 5]; // -1
+
+console.log(alternatingSubarray(nums));
