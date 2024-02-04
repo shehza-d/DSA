7604b7117d31cc8ed2ff07076aac8aa56206f571
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-02-04 16:54:18 +0500

    Update 05.cashRegister.js

--- 05.FreeCodeCamp/05.cashRegister.js
+++ 05.FreeCodeCamp/05.cashRegister.js
@@ -1,48 +1,94 @@
+const currencyUnit = {
+  PENNY: 1,
+  NICKEL: 5,
+  DIME: 10,
+  QUARTER: 25,
+  ONE: 100,
+  FIVE: 500,
+  TEN: 1000,
+  TWENTY: 2000,
+  "ONE HUNDRED": 10_000,
+};
+
 function checkCashRegister(price, cash, cid) {
-  let change = [];
   let status = "OPEN";
+  let change = [];
 
-  const moneyValue = {
-    PENNY: 0.01,
-    NICKEL: 0.05,
-    DIME: 0.1,
-    QUARTER: 0.25,
-    ONE: 1,
-    FIVE: 5,
-    TEN: 10,
-    TWENTY: 20,
-  };
+  let changeToReturn = cash * 100 - price * 100;
+  const changeToReturnCopy = changeToReturn;
 
-  for (let i = cid.length - 1; i >= 0; i--) {
-    console.log(cid[i]);
-  }
+  let totalAmountInCid = 0;
+  // removing 0 and reversing because we want to give big values first like hundred and twenty
+  const newCid = cid.filter((elem) => elem[1] !== 0).reverse();
 
-  return { status, change: [] };
+  newCid.forEach((item) => {
+    const currency = item[0];
+    let currencyAmount = item[1] * 100;
+    totalAmountInCid += currencyAmount;
+
+    let amount = 0;
+    const currencyValue = currencyUnit[currency];
+
+    while (changeToReturn >= currencyValue && currencyAmount > 0) {
+      amount += currencyValue;
+      changeToReturn -= currencyValue;
+      currencyAmount -= currencyValue;
+    }
+    if (amount) change.push([currency, amount / 100]);
+  });
+
+  if (changeToReturn > 0) {
+    status = "INSUFFICIENT_FUNDS";
+    change = [];
+  } else if (changeToReturn === 0 && changeToReturnCopy === totalAmountInCid) {
+    status = "CLOSED";
+    change = cid;
+  } else status = "OPEN";
+
+  return { status, change };
 }
+
+// cid = cash in drawer
+
+// console.log(
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]); //{ status: 'OPEN', change: [ [ 'QUARTER', 0.5 ] ] }
+// );
 
 console.log(
   checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100],
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
   ])
 );
 
-// should return {
-//   status: "OPEN",
-//   change: [
-//     ["TWENTY", 60],
-//     ["TEN", 20],
-//     ["FIVE", 15],
-//     ["ONE", 1],
-//     ["QUARTER", 0.5],
-//     ["DIME", 0.2],
-//     ["PENNY", 0.04],
-//   ],
-// };
+result = {
+  status: "CLOSED",
+  change: [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
+  ],
+};
