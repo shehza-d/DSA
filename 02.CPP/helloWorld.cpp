8bdea3d72d2a84d060c359335cebf1dc59c7d8f7
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-26 03:25:56 +0500

    cpp cin

--- 02.CPP/helloWorld.cpp
+++ 02.CPP/helloWorld.cpp
@@ -4,5 +4,15 @@ using namespace std;
 int main()
 {
 	cout << "Assalam o alaikum world" << endl;
+	cout << "konain raza" << endl;
+
+	int a;
+	int b;
+
+	cin >> a;
+	cin >> b;
+
+	cout << " a + b = " << a + b;
+	
 	return 0;
 }
