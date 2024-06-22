2a38ca2c7c9263d282dc42bed1e724bd03cd35b5
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-22 20:42:13 +0500

    Create find-minimum-operations-to-make-all-elements-divisible-by-three.ts

---
+++ 11.Contest/11.biweekly-contest-133/find-minimum-operations-to-make-all-elements-divisible-by-three.ts
@@ -0,0 +1,16 @@
+const minimumOperations = (nums: number[]): number => {
+  let operations = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    if (number % 3 === 0) continue; // if the number is already in 3s table we'll skip to next number
+    if ((number + 1) % 3 === 0) operations++; // adding 1 to current number and checking if it is devisable by now or not
+    if ((number - 1) % 3 === 0) operations++; // subtracting 1 to current number and checking if it is devisable by now or not
+  }
+  return operations;
+};
+
+const nums = [1, 2, 3, 4]; // 3
+const nums = [3, 6, 9]; // 0
+
+console.log(minimumOperations(nums));
