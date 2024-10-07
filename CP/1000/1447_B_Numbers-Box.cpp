541d2aa790905f104490bb9631cf48c9e78958b4
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-10-07 10:12:31 +0500

    :+1: CP setup done in 800 folder
    https://youtu.be/QYO3aCP1Gmk?si=knceleVzj3ZcPfYc

--- CP/1000/1447_B_Numbers-Box.cpp
+++ CP/1000/1447_B_Numbers-Box.cpp
@@ -4,31 +4,12 @@ using namespace std;
 
 int main()
 {
-	int t, n, m;
+	int t;
 	cin >> t;
 
 	while (t--)
 	{
-		cin >> n >> m;
-		int neg = 0, sum = 0, Min = 101;
-
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				int temp;
-				cin >> temp;
-
-				sum += abs(temp);
-				if (temp < 0)
-					neg++;
-				Min = min(abs(temp), Min);
-			}
-		}
-		if (neg % 2 == 1)
-			cout << sum - (2 * Min) << endl;
-		else
-			cout << sum << endl;
+		cout <<"testin "<< t << endl;
 	}
 
 	return 0;
---
+++ CP/800/1903_A_Halloumi_Boxes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cout << "testing3 " << t << endl;
+    }
+
+    return 0;
+}
