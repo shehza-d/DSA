bc6bb2ba6e589baff1e150ce79bdd9f2710d64f4
shehzad iqbal <shehzaddiqbal@gmail.com>
2025-01-19 23:22:52 +0500

    sunday printing patterns striver
    live: https://youtube.com/live/ntMwdzgG8ao?feature=share

---
+++ 00.Striver-A2Z/patterns/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+// https://youtube.com/live/ntMwdzgG8ao?feature=share
+
+void pattern1(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern2(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern3(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << j + 1 << " ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern4(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << i + 1 << " ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern5(int n)
+{
+	for (int i = n; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*" << " ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern6(int n)
+{
+	for (int i = n; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << j + 1 << " ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern7(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < (i * 2) + 1; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+void pattern8(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < (i * 2) + 1; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	// pattern1(6);
+	// pattern2(3);
+	// pattern3(5);
+	// pattern4(5);
+	// pattern5(6);
+	// pattern6(5);
+	// pattern7(3);
+	pattern8(3);
+}
+// https://youtube.com/playlist?list=PL2sQdFoGnLIgTJ4hP3ab4HoPOe4BT4ose&si=NAJw9Ti_sH0yVvK2
